37dbcba4f2a94df3a3f9db420817ed659b99c14a
Abdelrahman Zaki <me@abdelrahmanzaki.com>
2025-05-29 19:47:04 +0300

    testing

--- src/main.tsx
+++ src/main.tsx
@@ -9,8 +9,8 @@ createRoot(document.getElementById("root")!).render(
     domain={import.meta.env.VITE_KINDE_DOMAIN}
     logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URL}
     redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URL}
-    // When running local against a custom domain, include the line below
-    // useInsecureForRefreshToken={true}
+  // When running local against a custom domain, include the line below
+  // useInsecureForRefreshToken={true}
   >
     <App />
   </KindeProvider>
